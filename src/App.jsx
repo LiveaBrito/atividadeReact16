6b9bd1a62f1858267b339badb07c04f43bb5dd4e
Livea Brito <159591382+LiveaBrito@users.noreply.github.com>
2024-10-20 03:17:45 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Site from './components/Site'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Site/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Site.jsx
@@ -0,0 +1,7 @@
+function Site(){
+
+    return <h1><a href="https://react.dev/" class="site">Clique aqui e Visite o site do ReactJs</a></h1>
+
+}
+
+export default Site
